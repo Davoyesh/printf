fdad91ba05cac2cfd96b32d3ef98a70f76958305
Davoyesh <oyindola.esho@gmail.com>
2023-04-23 07:49:12 -0700

    God abeg

---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+int _printf(const char *format, ...) {
+    va_list args;
+    int printed_chars = 0, buff_size = 0;
+    char buffer[BUFFER_SIZE], *str;
+
+    va_start(args, format);
+
+    for (int i = 0; format[i]; i++) {
+        if (format[i] != '%') {
+            if (buff_size == BUFFER_SIZE) {
+                write(1, buffer, buff_size);
+                printed_chars += buff_size;
+                buff_size = 0;
+            }
+            buffer[buff_size++] = format[i];
+            continue;
+        }
+
+        switch (format[++i]) {
+            case 'c':
+                if (buff_size == BUFFER_SIZE) {
+                    write(1, buffer, buff_size);
+                    printed_chars += buff_size;
+                    buff_size = 0;
+                }
+                buffer[buff_size++] = va_arg(args, int);
+                break;
+
+            case 's':
+                str = va_arg(args, char*);
+                for (int j = 0; str[j]; j++) {
+                    if (buff_size == BUFFER_SIZE) {
+                        write(1, buffer, buff_size);
+                        printed_chars += buff_size;
+                        buff_size = 0;
+                    }
+                    buffer[buff_size++] = str[j];
+                }
+                break;
+
+            case '%':
+                if (buff_size == BUFFER_SIZE) {
+                    write(1, buffer, buff_size);
+                    printed_chars += buff_size;
+                    buff_size = 0;
+                }
+                buffer[buff_size++] = '%';
+                break;
+
+            default:
+                return (-1);
+        }
+    }
+
+    va_end(args);
+
+    if (buff_size) {
+        write(1, buffer, buff_size);
+        printed_chars += buff_size;
+    }
+
+    return (printed_chars);
+}
